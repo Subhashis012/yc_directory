90e832cb7796db83fd1ab140a61d118b20b60c1b
Subhashis <subhashisdhara123@gmail.com>
2024-10-29 13:41:55 +0530

    added

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,26 +1,40 @@
-import { formatDate } from "@/lib/utils";
-import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
-import Image from "next/image";
-import Link from "next/link";
-import { notFound } from "next/navigation";
-import markdownit from "markdown-it";
 import { Suspense } from "react";
+import { client } from "@/sanity/lib/client";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/utils";
+import Link from "next/link";
+import Image from "next/image";
+
+import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
 
 export const experimental_ppr = true;
 
-const page = async ({ params }: { params: Promise<{ id: string }> }) => {
-  const id = (await params).id;
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const resolvedParams = await params;
+  if (!resolvedParams?.id) {
+    throw new Error("ID not found in params.");
+  }
+  const id = resolvedParams.id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, editorResponse] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks-new" }),
+  ]);
 
   if (!post) return notFound();
 
+  const editorPosts = editorResponse?.select || [];
   const parsedContent = md.render(post?.pitch || "");
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
@@ -35,10 +49,11 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
           alt="thumbnail"
           className="w-full h-auto rounded-xl"
         />
+
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
             <Link
-              href={`/users/${post.author?._id}`}
+              href={`/user/${post.author?._id}`}
               className="flex gap-2 items-center mb-3"
             >
               <Image
@@ -48,7 +63,6 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 height={64}
                 className="rounded-full drop-shadow-lg"
               />
-
               <div>
                 <p className="text-20-medium">{post.author.name}</p>
                 <p className="text-16-medium !text-black-300">
@@ -56,8 +70,10 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 </p>
               </div>
             </Link>
+
             <p className="category-tag">{post.category}</p>
           </div>
+
           <h3 className="text-30-bold">Pitch Details</h3>
           {parsedContent ? (
             <article
@@ -71,6 +87,16 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
+        {editorPosts.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
 
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
           <View id={id} />
@@ -80,4 +106,4 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   );
 };
 
-export default page;
+export default Page;
