24f80dd8731661a7b7b3b68c0fae0bf92db3d89f
Subhashis <subhashisdhara123@gmail.com>
2024-10-29 11:47:34 +0530

    Create Section Added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
+import SearchForm from "@/components/SearchForm";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
-import SearchForm from "../../components/SearchForm";
-import { STARTUP_QUERY } from "@/sanity/lib/queries";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({
   searchParams,
@@ -9,26 +10,35 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const params = {search: query || null};
+  const params = { search: query || null };
 
-  const {data:posts} = await sanityFetch({query: STARTUP_QUERY, params});
-  
+  const session = await auth();
+
+  console.log(session?.id);
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch Your Startup, <br /> Connect With Entrepreneurs
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
         </h1>
-        <p className="sub-heading !mx-w-3xl">
+
+        <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
+
         <SearchForm query={query} />
       </section>
+
       <section className="section_container">
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
+
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
@@ -39,6 +49,7 @@ export default async function Home({
           )}
         </ul>
       </section>
+
       <SanityLive />
     </>
   );
